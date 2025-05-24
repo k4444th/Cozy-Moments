9b8671b68c8caaa825f843e1465f795866255ebe
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-05-24 17:33:55 +0200

    Move Sidenav into everything group for easier movement

--- scripts/sidenav.gd
+++ scripts/sidenav.gd
@@ -6,13 +6,14 @@ var sidenavWidth := 250.0
 var sidenavContentWidth := 200
 var openSidenavSpeed := 0.35
 
-@onready var sidenav = $Sidenav
+@onready var everything = $Everything
+@onready var sidenav = $Everything/Sidenav
 @onready var camera = $"../Camera2D"
-@onready var arrowButton = $ArrowButton
-@onready var calendar = $Sidenav/CalendarButton
+@onready var arrowButton = $Everything/ArrowButton
+@onready var calendar = $Everything/CalendarButton
 
-func _ready() -> void:
-	sidenav.size.x = sidenavWidth
+func _ready() -> void:	
+	sidenav.size = Vector2(sidenavWidth, DisplayServer.screen_get_size().y)
 	arrowButton.position = Vector2(sidenavWidth + buttonMargin, buttonMargin)
 	calendar.scale = Vector2(sidenavContentWidth / 256.0, sidenavContentWidth / 256.0)
 	calendar.position = Vector2((sidenavWidth - sidenavContentWidth) / 2, (sidenavWidth - sidenavContentWidth) / 2)
@@ -21,29 +22,24 @@ func _ready() -> void:
 	
 	if sidenavOpen:
 		camera.position.x = -(sidenavWidth / 2)
-		sidenav.position.x = 0.0
-		arrowButton.position.x = sidenavWidth + buttonMargin
+		everything.position.x = 0.0
 	else:
 		camera.position.x = 0
-		sidenav.position.x = -sidenavWidth
-		arrowButton.position.x = buttonMargin
+		everything.position.x = -sidenavWidth
 
 func _on_arrow_button_pressed() -> void:
 	sidenavOpen = !sidenavOpen
 	arrowButton.flip_h = !sidenavOpen
 	
 	var cameraTween = get_tree().create_tween()
-	var buttonTween = get_tree().create_tween()
-	var sidenavTween = get_tree().create_tween()
+	var everythingTween = get_tree().create_tween()
 	
 	if sidenavOpen:
 		cameraTween.tween_property(camera, "position:x", -(sidenavWidth / 2), openSidenavSpeed)
-		buttonTween.tween_property(arrowButton, "position:x", sidenavWidth + buttonMargin, openSidenavSpeed)
-		sidenavTween.tween_property(sidenav, "position:x", 0.0, openSidenavSpeed)
+		everythingTween.tween_property(everything, "position:x", 0.0, openSidenavSpeed)
 	else:
 		cameraTween.tween_property(camera, "position:x", 0.0, openSidenavSpeed)
-		buttonTween.tween_property(arrowButton, "position:x", buttonMargin, openSidenavSpeed)
-		sidenavTween.tween_property(sidenav, "position:x", -sidenavWidth, openSidenavSpeed)
+		everythingTween.tween_property(everything, "position:x", -sidenavWidth, openSidenavSpeed)
 
 func _on_calendar_button_pressed() -> void:
 	print("Next Round")
