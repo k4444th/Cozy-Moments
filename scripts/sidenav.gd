42622c292b7f545734b417ba52a5694fa29a80c8
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-05-24 13:13:36 +0200

    Dynamic Sidenav Width

--- scripts/sidenav.gd
+++ scripts/sidenav.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+var buttonMargin := 15.0
+var sidenavWidth := 250.0
 var sidenavOpen := false
 var openSidenavSpeed := 0.35
 
@@ -8,16 +10,19 @@ var openSidenavSpeed := 0.35
 @onready var arrowButton = $ArrowButton
 
 func _ready() -> void:
+	sidenav.size.x = sidenavWidth
+	arrowButton.position = Vector2(sidenavWidth + buttonMargin, buttonMargin)
+	
 	arrowButton.flip_h = !sidenavOpen
 	
 	if sidenavOpen:
-		camera.position.x = -125.0
+		camera.position.x = -(sidenavWidth / 2)
 		sidenav.position.x = 0.0
-		arrowButton.position.x = 265.0
+		arrowButton.position.x = sidenavWidth + buttonMargin
 	else:
 		camera.position.x = 0
-		sidenav.position.x = -250.0
-		arrowButton.position.x = 15.0
+		sidenav.position.x = -sidenavWidth
+		arrowButton.position.x = buttonMargin
 
 func _on_arrow_button_pressed() -> void:
 	sidenavOpen = !sidenavOpen
@@ -28,10 +33,10 @@ func _on_arrow_button_pressed() -> void:
 	var sidenavTween = get_tree().create_tween()
 	
 	if sidenavOpen:
-		cameraTween.tween_property(camera, "position:x", -125.0, openSidenavSpeed)
-		buttonTween.tween_property(arrowButton, "position:x", 265.0, openSidenavSpeed)
+		cameraTween.tween_property(camera, "position:x", -(sidenavWidth / 2), openSidenavSpeed)
+		buttonTween.tween_property(arrowButton, "position:x", sidenavWidth + buttonMargin, openSidenavSpeed)
 		sidenavTween.tween_property(sidenav, "position:x", 0.0, openSidenavSpeed)
 	else:
 		cameraTween.tween_property(camera, "position:x", 0.0, openSidenavSpeed)
-		buttonTween.tween_property(arrowButton, "position:x", 15.0, openSidenavSpeed)
-		sidenavTween.tween_property(sidenav, "position:x", -250.0, openSidenavSpeed)
+		buttonTween.tween_property(arrowButton, "position:x", buttonMargin, openSidenavSpeed)
+		sidenavTween.tween_property(sidenav, "position:x", -sidenavWidth, openSidenavSpeed)
