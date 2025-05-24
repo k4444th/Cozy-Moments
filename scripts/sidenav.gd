353098da26c8b9529aea53993cf5496f8dcee77a
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-05-24 12:50:45 +0200

    Smooth Sidenav Position Transition

--- scripts/sidenav.gd
+++ scripts/sidenav.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 var sidenavOpen := true
+var openSidenavSpeed := 0.35
 
 @onready var sidenav = $Sidenav
 @onready var arrowButton = $ArrowButton
@@ -9,9 +10,16 @@ func _on_arrow_button_pressed() -> void:
 	sidenavOpen = !sidenavOpen
 	arrowButton.flip_h = !sidenavOpen
 	
+	var sidenavTween = get_tree().create_tween()
+	var buttonTween = get_tree().create_tween()
+	
 	if sidenavOpen:
-		sidenav.position.x = 0.0
-		arrowButton.position.x = 265.0
+		sidenavTween.tween_property(sidenav, "position:x", 0.0, openSidenavSpeed)
+		buttonTween.tween_property(arrowButton, "position:x", 265.0, openSidenavSpeed)
+		#sidenav.position.x = 0.0
+		#arrowButton.position.x = 265.0
 	else:
-		sidenav.position.x = -250.0
-		arrowButton.position.x = 15.0
+		sidenavTween.tween_property(sidenav, "position:x", -250.0, openSidenavSpeed)
+		buttonTween.tween_property(arrowButton, "position:x", 15.0, openSidenavSpeed)
+		#sidenav.position.x = -250.0
+		#arrowButton.position.x = 15.0
