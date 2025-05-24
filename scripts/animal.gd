1a94493126e115f0d7b7f18734538ad18b9fbc8c
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-05-24 11:33:09 +0200

    Snap animal to workstation

--- scripts/animal.gd
+++ scripts/animal.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
-var mouseOver = false
-var dragging = false
+var dragging := false
+var overlap := false
+var overlapCoords: Vector2
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -10,3 +11,16 @@ func _input(event):
 	
 	if event is InputEventMouseMotion and dragging:
 		global_position += event.relative
+
+func _process(_delta: float) -> void:
+	if overlap and !dragging:
+		global_position = overlapCoords
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.name ==  "Workstation":
+		overlap = true
+		overlapCoords = area.global_position
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.name ==  "Workstation":
+		overlap = false
