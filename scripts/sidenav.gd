6aaf1fd502d711dd6c3beaea0a5f415f2495faab
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-05-24 13:09:50 +0200

    Change Camera Position when opening Sidenav

--- scripts/sidenav.gd
+++ scripts/sidenav.gd
@@ -1,25 +1,37 @@
 extends CanvasLayer
 
-var sidenavOpen := true
+var sidenavOpen := false
 var openSidenavSpeed := 0.35
 
+@onready var camera = $"../Camera2D"
 @onready var sidenav = $Sidenav
 @onready var arrowButton = $ArrowButton
+
+func _ready() -> void:
+	arrowButton.flip_h = !sidenavOpen
+	
+	if sidenavOpen:
+		camera.position.x = -125.0
+		sidenav.position.x = 0.0
+		arrowButton.position.x = 265.0
+	else:
+		camera.position.x = 0
+		sidenav.position.x = -250.0
+		arrowButton.position.x = 15.0
 
 func _on_arrow_button_pressed() -> void:
 	sidenavOpen = !sidenavOpen
 	arrowButton.flip_h = !sidenavOpen
 	
-	var sidenavTween = get_tree().create_tween()
+	var cameraTween = get_tree().create_tween()
 	var buttonTween = get_tree().create_tween()
+	var sidenavTween = get_tree().create_tween()
 	
 	if sidenavOpen:
-		sidenavTween.tween_property(sidenav, "position:x", 0.0, openSidenavSpeed)
+		cameraTween.tween_property(camera, "position:x", -125.0, openSidenavSpeed)
 		buttonTween.tween_property(arrowButton, "position:x", 265.0, openSidenavSpeed)
-		#sidenav.position.x = 0.0
-		#arrowButton.position.x = 265.0
+		sidenavTween.tween_property(sidenav, "position:x", 0.0, openSidenavSpeed)
 	else:
-		sidenavTween.tween_property(sidenav, "position:x", -250.0, openSidenavSpeed)
+		cameraTween.tween_property(camera, "position:x", 0.0, openSidenavSpeed)
 		buttonTween.tween_property(arrowButton, "position:x", 15.0, openSidenavSpeed)
-		#sidenav.position.x = -250.0
-		#arrowButton.position.x = 15.0
+		sidenavTween.tween_property(sidenav, "position:x", -250.0, openSidenavSpeed)
