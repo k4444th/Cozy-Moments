9ea5a63150a587b7d0f7c451a76a19b810855357
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-05-25 10:28:47 +0200

    Add ressources to GUI

---
+++ scripts/gamemanager.gd
@@ -0,0 +1,46 @@
+extends Node
+
+const SAVE_FILE_PATH := "user://cozymoments.json"
+
+const ressources := ["berries", "wood", "tea"]
+
+var currentRound := {
+	"memories": 0,
+	"berries": 0,
+	"wood": 0,
+	"tea": 0
+}
+
+func _ready() -> void:
+	loadGame()
+
+func saveGame():
+	var data = {
+		"currentRound": currentRound
+	}
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_string(JSON.stringify(data))
+	file.close()
+
+func loadGame():
+	if FileAccess.file_exists(SAVE_FILE_PATH):
+		var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+		var content = file.get_as_text()
+		file.close()
+
+		var loadedData = JSON.parse_string(content)
+		if typeof(loadedData) == TYPE_DICTIONARY:
+			currentRound = loadedData.get("currentRound", {
+					"memories": 0,
+					"berries": 0,
+					"wood": 0,
+					"tea": 0
+				}
+			)
+
+func resetGame():
+	currentRound["memories"] = 0
+	currentRound["berries"] = 0
+	currentRound["wood"] = 0
+	currentRound["tea"] = 0
+	saveGame()
--- scripts/sidenav.gd
+++ scripts/sidenav.gd
@@ -5,19 +5,31 @@ var buttonMargin := 15.0
 var sidenavWidth := 250.0
 var sidenavContentWidth := 200
 var openSidenavSpeed := 0.35
+var availableRessources = 0
 
+@onready var camera = $"../Camera2D"
 @onready var everything = $Everything
 @onready var sidenav = $Everything/Sidenav
-@onready var camera = $"../Camera2D"
 @onready var arrowButton = $Everything/ArrowButton
 @onready var calendar = $Everything/CalendarButton
+@onready var ressources = $Everything/Ressources
 
-func _ready() -> void:	
-	sidenav.size = Vector2(sidenavWidth, DisplayServer.screen_get_size().y)
+@onready var ressourceScene : = preload("res://scenes/ressource.tscn")
+
+func _ready() -> void:
+	get_viewport().size_changed.connect(resize)
+	initSizes()
+	initPositions()
+	initRessources()
+
+func initSizes():
+	sidenav.size = Vector2(sidenavWidth, get_viewport().get_visible_rect().size.y)
 	arrowButton.position = Vector2(sidenavWidth + buttonMargin, buttonMargin)
 	calendar.scale = Vector2(sidenavContentWidth / 256.0, sidenavContentWidth / 256.0)
 	calendar.position = Vector2((sidenavWidth - sidenavContentWidth) / 2, (sidenavWidth - sidenavContentWidth) / 2)
-	
+	ressources.position = Vector2(sidenavWidth + buttonMargin, get_viewport().get_visible_rect().size.y - 64 - buttonMargin)
+
+func initPositions():
 	arrowButton.flip_h = !sidenavOpen
 	
 	if sidenavOpen:
@@ -27,6 +39,23 @@ func _ready() -> void:
 		camera.position.x = 0
 		everything.position.x = -sidenavWidth
 
+func initRessources():
+	print(Gamemanager.ressources)
+	for ressource in Gamemanager.ressources:
+		if Gamemanager.currentRound[ressource] > 0:
+			addRessourceToGUI(ressource, availableRessources)
+			availableRessources += 1
+
+func addRessourceToGUI(ressource: String, index: int):
+	var ressourceNode = ressourceScene.instantiate()
+	ressourceNode.position.x = index * (128 + buttonMargin)
+	ressourceNode.get_child(1).text = str(int(Gamemanager.currentRound[ressource]))
+	get_node("/root/Game/Sidenav/Everything/Ressources").add_child(ressourceNode)
+
+func resize():
+	sidenav.size = Vector2(sidenavWidth, get_viewport().get_visible_rect().size.y)
+	ressources.position = Vector2(sidenavWidth + buttonMargin, get_viewport().get_visible_rect().size.y - 64 - buttonMargin)
+	
 func _on_arrow_button_pressed() -> void:
 	sidenavOpen = !sidenavOpen
 	arrowButton.flip_h = !sidenavOpen
